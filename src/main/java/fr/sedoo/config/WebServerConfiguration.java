d4f757ecdebf9d249ed290c864d8f56e88914c14
Francois ANDRE <francois.andre@obs-mip.fr>
2022-10-14 15:00:07 +0200

    Fix: Add Max header size in config

---
+++ src/main/java/fr/sedoo/config/WebServerConfiguration.java
@@ -0,0 +1,19 @@
+package fr.sedoo.config;
+
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerConfiguration implements WebServerFactoryCustomizer<NettyReactiveWebServerFactory> {
+
+    // The default value is 8192 (8K) but may result in 413 when header is too lager.
+    // Enlarge the header size to 16384 (16K) which is enough for most cases.
+    private static final int MAX_LINE_SIZE = 163840000;
+    private static final int MAX_HEADER_SIZE = 163840000;
+
+    public void customize(NettyReactiveWebServerFactory factory) {
+        factory.addServerCustomizers(server ->
+                server.httpRequestDecoder(decoder -> decoder.maxInitialLineLength(MAX_LINE_SIZE).maxHeaderSize(MAX_HEADER_SIZE)));
+    }
+}
